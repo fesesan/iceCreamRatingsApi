a78465ced2987883ab622b5d2fa63189050d23d4
Felipe Selis Santana <felipe.selis@telefonica.com>
2022-06-08 16:53:39 -0300

    criacao do getRating

---
+++ getRating/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    id = req.params.get('id')
+
+    if not doc:
+        return func.HttpResponse('rating not found', status_code=404)
+    else:
+        payload_response = {
+            "id": doc[0]['id'],
+            "userId": doc[0]['userId'],
+            "productId": doc[0]['productId'],
+            "timestamp": doc[0]['timestamp'],
+            "locationName": doc[0]['locationName'],
+            "rating": doc[0]['rating'],
+            "userNotes": doc[0]['userNotes']
+        }
+        return func.HttpResponse(json.dumps(payload_response), status_code=200)
