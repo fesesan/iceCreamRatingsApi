a3c9ac0741e150b254a4edc35f1b75e88da25919
Felipe Selis Santana <felipe.selis@telefonica.com>
2022-06-08 14:37:01 -0300

    criacao post rating

--- createRating/__init__.py
+++ createRating/__init__.py
@@ -3,6 +3,8 @@ import logging
 import requests
 import uuid
 
+from time import gmtime, strftime
+
 import azure.functions as func
 
 def main(req: func.HttpRequest,  doc: func.Out[func.Document]) -> func.HttpResponse:
@@ -10,51 +12,68 @@ def main(req: func.HttpRequest,  doc: func.Out[func.Document]) -> func.HttpRespo
 
     user = None
     product = None
+    rating = None
     try:
         req_body = req.get_json()
         user_id = req_body.get('userId')
         product_id = req_body.get('productId')
+        rating = req_body.get('rating')
 
         validator = {
-            "not found": []
+            "errors": []
         }
 
         if not user_id:
-            validator["not found"].append({'param': 'user_id'})
+            validator["errors"].append({'param': 'user_id'})
         else:
-            user = find_user(user_id).json()['userId']
-            if not user:
-                validator["not found"].append({'user does not exist'})
+            response = find_user(user_id)
+            if response.status_code == 400:
+                validator["errors"].append({'message':'user does not exist'})
+            else:
+                user = response.json()['userId']
 
         if not product_id:
-            validator["not found"].append({'param': 'product_id'})
+            validator["errors"].append({'param': 'product_id'})
         else:
-            product = find_user(product_id).json()['productId']
-            if not user:
-                validator["not found"].append({'product does not exist'})
+            response = find_product(product_id)
+            if response.status_code == 400:
+                validator["errors"].append({'message': 'product does not exist'})
+            else:
+                product = response.json()['productId']
         
+        if not rating:
+            validator["errors"].append({'param': 'rating'})
+        else:
+            if not validate_rating_range(rating):
+                print('nao validou')
+                validator["errors"].append({'message': 'rating 0 to 5'})        
+
     except ValueError:
-        pass
+        print('erro ao converter')
 
-    payload_response = {
-        "id": str(uuid.uuid4()),
-        "userId": user,
-        "productId": product,
-        "timestamp": "2018-05-21 21:27:47Z",
-        "locationName": "teste felipe",
-        "rating": 5,
-        "userNotes": "I love the subtle notes of orange in this ice cream!"
-    }
-
-    if user_id and product_id:
-
+    if not validator['errors']:
+        payload_response = {
+            "id": str(uuid.uuid4()),
+            "userId": user,
+            "productId": product,
+            "timestamp": strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+            "locationName": "Sample ice cream shop",
+            "rating": str(rating),
+            "userNotes": "I love the subtle notes of orange in this ice cream!"
+        }
         doc.set(func.Document.from_json(json.dumps(payload_response)))
         return func.HttpResponse(json.dumps(payload_response), status_code=200)
     else:
+        print(validator)
         return func.HttpResponse(
              json.dumps(validator),
              status_code=400
         )
+
+def validate_rating_range(rating):
+    print('valor rating ' + str(rating))   
+    return rating in [0,1,2,3,4,5]
+
 
 def find_user(user_id):
     return requests.get(f'https://serverlessohapi.azurewebsites.net/api/GetUser?userId={user_id}')
