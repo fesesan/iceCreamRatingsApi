e3fb235c1cddf953eb5bd61f6a26db5b33cb8891
Felipe Selis Santana <felipe.selis@telefonica.com>
2022-06-08 09:40:29 -0300

    obtendo user_id e product_id

--- createRating/__init__.py
+++ createRating/__init__.py
@@ -8,17 +8,18 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    try:
+        req_body = req.get_json()
+        user_id = req_body.get('userId')
+        product_id = req_body.get('productId')
+    except ValueError:
+        pass
+    else:
+        name = req_body.get('name')
+
+    if user_id and product_id:
+        return func.HttpResponse(f"Hello, {user_id} and {product_id}. This HTTP triggered function executed successfully.")
     else:
         response = requests.get('https://serverlessohapi.azurewebsites.net/api/GetProducts').json()
         logging.info(response)
@@ -26,3 +27,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              'teste ok',
              status_code=200
         )
+
+
+
+
+
+
+def validate_user(user_id):
+    pass
+
+def validate_product(product_id):
+    pass
